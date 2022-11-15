6c5b39c95240cf89d6ccc0d582371c9f16d90081
Oleg Popov <de.pa4inko@gmail.com>
2022-11-15 20:48:29 +0300

    feat: string component

--- src/components/string/string.tsx
+++ src/components/string/string.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { SolutionLayout } from "../ui/solution-layout/solution-layout";
+import { Input } from "../ui/input/input";
+import { Button } from "../ui/button/button";
+import { Circle } from "../ui/circle/circle";
 
 export const StringComponent: React.FC = () => {
   return (
     <SolutionLayout title="Строка">
-     
+      <form className="">
+        <Input />
+        <Button />
+      </form>
+      <div>
+        <Circle />
+      </div>
     </SolutionLayout>
   );
 };
